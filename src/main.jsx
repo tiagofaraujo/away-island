2134b038aec55c9d0d421f87f8f87972e122a1ec
Tiago Araujo <114108170+tiagofaraujo@users.noreply.github.com>
2026-08-31 23:27:45 +0100

    Load meme radar styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './styles/index.css';
 import './styles/lettering.css';
 import './styles/logo.css';
+import './styles/radar.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
