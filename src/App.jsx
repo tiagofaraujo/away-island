43fd4640547767590089c5ad4c03c8b0c5bccf7e
Tiago Araujo <114108170+tiagofaraujo@users.noreply.github.com>
2026-09-01 23:21:49 +0100

    Simplify site around AWAY Radar shortlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Story from './components/Story.jsx';
-import Terminal from './components/Terminal.jsx';
-import Signal from './components/Signal.jsx';
 import Radar from './components/Radar.jsx';
+import MemeShortlist from './components/MemeShortlist.jsx';
 import RadarLab from './components/RadarLab.jsx';
 import ExitPlan from './components/ExitPlan.jsx';
 import Community from './components/Community.jsx';
@@ -17,9 +16,8 @@ function App() {
       <Navbar />
       <Hero />
       <Story />
-      <Terminal />
-      <Signal />
       <Radar />
+      <MemeShortlist />
       <RadarLab />
       <ExitPlan />
       <Community />
