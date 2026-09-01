5ffe4bd60ac4ff4937a729970c013b0a52f99c36
Tiago Araujo <114108170+tiagofaraujo@users.noreply.github.com>
2026-09-01 23:22:57 +0100

    Import shortlist styling

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles/index.css';
 import './styles/lettering.css';
 import './styles/logo.css';
 import './styles/radar.css';
+import './styles/meme-shortlist.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
